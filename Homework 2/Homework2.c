53b35544777e15dec88fc6f9d9ed85ca04691674
Murdockbleak <51054573+murdockbleak@users.noreply.github.com>
2020-05-06 16:07:03 +0300

    Update Homework2.c

--- Homework 2/Homework2.c
+++ Homework 2/Homework2.c
@@ -5,36 +5,37 @@
 #include<unistd.h> 
 
   
-int main() 
+int main(int argc, char *argv[]) 
 {  
-    int p[2]; 
+    int p[2];    
     pid_t pid; 
-  	int n;
+  	char *a = argv[1]; 
+    int n = atoi(a);
     if (pipe(p) < 0) 
         exit(1); 
     if ((pid = fork()) > 0)
     {   
-    	close(p[0]);
-    	printf("give a number:");
-    	scanf("%d", &n);
-      write(p[1], &n, 100); 
-      close(p[1]); 
-      wait(NULL); 
+        close(p[0]);
+    	//printf("give a number:");
+    	//atoi(argv[1],"%d", &n);
+        write(p[1], &n, 100); 
+        close(p[1]); 
+        wait(NULL);  
     } 
   
-    else 
+    else if (pid ==0)
     { 
-      close(p[1]);
+        close(p[1]);
         //int n; 
-      float sum;
-      read(p[0], &n, 100);
-      int sign = 1;
+        float sum;
+        read(p[0], &n, 100);
+        int sign = 1;
     	for (int i = 0; i < n; ++i) 
     	{           
         sum += sign/(2.0*i+1.0);
         sign *= -1;
     	}
-        printf("%lf",4*sum);
-    } 
+        printf("%lf\n",4*sum);
+    }  
     return 0; 
 } 
